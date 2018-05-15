758a89bd5be2eb589c12eef948c8cd0093c2a79e
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-15 16:53:57 +0800

    Initialize tree.js

---
+++ tree.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(msgs, endState, specificness){
+    this.msgs = msgs;
+    this.endState = endState;
+    this.specificness = specificness;
+  }
+
+  // getters
+  get msgs() {
+    return this.msgs;
+  }
+  get endState() {
+    return this.endState;
+  }
+  get specificness() {
+    return this.specificness;
+  }
+
+  // setters
+  set setMsgs(inpmsgs) {
+    this.msgs = inpmsgs;
+  }
+  set setEndState(inpendState) {
+    this.endState = inpendState;
+  }
+  set setSpecificness(inpspecificness) {
+    this.specificness = inpspecificness;
+  }
+}
+
+
+function dfs(node){
+  node.msgs.push(node.endState.choices);
+  node.specificness = node.specificness+1;
+  for(var i=0; i<node.endState.nextStates; i++)
+  {
+    if(node.endState.nextStates[i]!=topConv)
+    {
+      var newNode = new Node(node.msgs,node.endState.nextStates[i],
+        node.specificness);
+      dfs(newNode);
+    }
+  }
+  return node;
+}
+
+
+curState = topConv;
