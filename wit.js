b245bac8f58dd07279336d90a0c62eb7391d7576
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-16 15:19:04 +0800

    Implemented doResponse: not tested yet

--- wit.js
+++ wit.js
@@ -1,21 +1,57 @@
-function getResponse (text, callback) {
+function getResponse (textInput, curState, callback) {
   $.ajax({
     url: 'https://api.wit.ai/message',
     data: {
-      'q': text,
+      'q': textInput,
       'access_token' : '67453HLLYIEHMNZPGKIVK6TOLP3GPGF4'
     },
     dataType: 'jsonp',
     method: 'GET',
     success:function(response) {
-              callback(response.entities);
+              callback(response.entities, curState);
             }
   });
 }
 
-function doResponse(resp) {
-    console.log(resp['opening_hours']);
-    console.log(resp['vacation_period'])
+function doResponse(entities, curState) {
+  var choiceLen = curState.getChoices.length;
+
+  var score = Array(choiceLen);
+  for(var i=0; i<choiceLen; i++) score[i]=0;
+
+  console.log(choiceLen);
+  console.log(entities);
+  
+  for(var i=0; i<choiceLen; i++)
+  {
+    console.log(curState.getChoices[i]);
+    if(typeof entities[curState.getChoices[i]] != "undefined")
+    {
+      score[i] += entities[curState.getChoices[i]]['confidence'];
+    }
+  }
+  console.log(score);
+
+  var maxVal=0.2;
+  var maxInd=-1;
+  for(var i=0; i<choiceLen; i++)
+  {
+    if(score[i]>maxVal)
+    {
+      maxVal = score[i];
+      maxInd = i;
+    }
+  }
+  if(maxInd == -1)
+  {
+    botResponse();
+    return;
+  }
+  curState = curState.getNextStates[maxInd];
+  doResponse(entities, curState);
 }
 
-getResponse("library open in holiday", doResponse);
+textInput = "Library open";
+curState=topConv;
+
+getResponse(textInput, curState, doResponse);
