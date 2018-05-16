0ff10273e73e30a6ba831b30dc884a1f4875e281
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-16 14:36:24 +0800

    Able to make the wit.js work, finally

--- wit.js
+++ wit.js
@@ -1,12 +1,21 @@
-$.ajax({
-  url: 'https://api.wit.ai/message',
-  data: {
-    'q': 'set an alarm in 10min',
-    'access_token' : '67453HLLYIEHMNZPGKIVK6TOLP3GPGF4'
-  },
-  dataType: 'jsonp',
-  method: 'GET',
-  success: function(response) {
-      console.log("wit success!", response);
-  }
-});
+function getResponse (text, callback) {
+  $.ajax({
+    url: 'https://api.wit.ai/message',
+    data: {
+      'q': text,
+      'access_token' : '67453HLLYIEHMNZPGKIVK6TOLP3GPGF4'
+    },
+    dataType: 'jsonp',
+    method: 'GET',
+    success:function(response) {
+              callback(response.entities);
+            }
+  });
+}
+
+function doResponse(resp) {
+    console.log(resp['opening_hours']);
+    console.log(resp['vacation_period'])
+}
+
+getResponse("library open in holiday", doResponse);
