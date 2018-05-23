3dada58ecd3c7d503bf80ac64c233d686352d835
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-23 11:08:04 +0800

    Updated data

--- states.js
+++ states.js
@@ -1,12 +1,5 @@
-const topConv = new State('Let\'s play a quiz',['Ok'],['curState = s1; var score=0;'],'');
-const s1 = new State('Rome is the capital of which country?',['Czech','Italy','Cuba','China'],['score += 0;','score+=1','score+=0','score+=1'],'curState = s2;');
-const s2 = new State('',,,'');
-const s3 = new State('',,,'');
-const s4 = new State('',,,'');
-const s5 = new State('',,,'');
-const s6 = new State('',,,'');
-const s7 = new State('',,,'');
-const s8 = new State('',,,'');
-const s9 = new State('',,,'');
-const s10 = new State('',,,'');
+const topConv = new State('Let\'s play a quiz',['Ok'],['curState = s1; score=0;'],'');
+const s1 = new State('Rome is the capital of which country?',['Czech','Italy','Cuba','China'],['score += 0;','score+=1;','score+=0;','score+=0;'],'curState = s2;');
+const s2 = new State('Moscow is the capital of which country?',['Czech', 'Germany', 'Sweden', 'Russia'],['score += 0;','score+=0;','score+=0;','score+=1;'],'curState = result;');
+const result = new State('Your scored',['Ok'],[''],'curState = topConv; addHistory(\'bot\',score.toString());');
 curState = topConv; botResponse();
