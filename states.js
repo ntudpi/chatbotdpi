a4c142f58949c2f4f1be32437bd0faac25f69516
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-23 11:33:37 +0800

    Update

--- states.js
+++ states.js
@@ -1,5 +1,6 @@
-const topConv = new State('Let\'s play a quiz',['Ok'],['curState = s1; score=0;'],'');
+const topConv = new State('Let\'s play a quiz',['Start'],['score=0;'],'curState = s1; ');
 const s1 = new State('Rome is the capital of which country?',['Czech','Italy','Cuba','China'],['score += 0;','score+=1;','score+=0;','score+=0;'],'curState = s2;');
 const s2 = new State('Moscow is the capital of which country?',['Czech', 'Germany', 'Sweden', 'Russia'],['score += 0;','score+=0;','score+=0;','score+=1;'],'curState = result;');
-const result = new State('Your scored',['Ok'],[''],'curState = topConv; addHistory(\'bot\',score.toString());');
+const result = new State('Click to show your score',['ShowScore'],[''],'curState = end; addHistory(\'bot\',score.toString());');
+const end = new State('Thanks for playing',[],[],'');
 curState = topConv; botResponse();
