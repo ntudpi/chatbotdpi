0044a96ab6eae647c36224dcc472fd959fa6d2dd
DPI@ntu.edu.sg <DPI@203-ASD01.DynIP.ntu.edu.sg>
2018-05-23 10:45:30 +0800

    Change due to change in always function

--- states.js
+++ states.js
@@ -1,2 +1,12 @@
-const topConv = new State('What is your name?',['1','2'],['console.log(\'1\');', 'console.log(\'2\');']);
+const topConv = new State('Let\'s play a quiz',['Ok'],['curState = s1; var score=0;'],'');
+const s1 = new State('Rome is the capital of which country?',['Czech','Italy','Cuba','China'],['score += 0;','score+=1','score+=0','score+=1'],'curState = s2;');
+const s2 = new State('',,,'');
+const s3 = new State('',,,'');
+const s4 = new State('',,,'');
+const s5 = new State('',,,'');
+const s6 = new State('',,,'');
+const s7 = new State('',,,'');
+const s8 = new State('',,,'');
+const s9 = new State('',,,'');
+const s10 = new State('',,,'');
 curState = topConv; botResponse();
